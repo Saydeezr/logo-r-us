9396041171d02634d89902885c219e907227c705
Saydeezr <saydeezr@gmail.com>
2024-05-13 13:12:13 -0400

    started test file

--- library/generateLogo.test.js
+++ library/generateLogo.test.js
@@ -1,3 +1,23 @@
 //const shape = new Triangle();
 //shape.setColor("blue");
 //expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+
+const createLogo = require('./library/generateLogo');
+
+describe('createLogo', () => {
+    test('if creates a logo with a circle shape', () => {
+      const data = {
+        initials: 'ABC',
+        textColor: 'white',
+        background: 'black',
+        shape: 'Circle'
+      };
+      const expectedCircle = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <circle fill="black" cx="150" cy="100" r="80"/>
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">ABC</text>
+      </svg>`;
+      expect(createLogo(data)).toBe(expectedCircle.trim());
+    });
+    test()
+})
