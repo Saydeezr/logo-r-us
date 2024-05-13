e0b2ff5746a6b7494b8bbc9ebfa8acdb82b085ac
Saydeezr <saydeezr@gmail.com>
2024-05-13 13:00:34 -0400

    changed file format and deleted unnecessary file

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const createLogo = require('./generateLogo')
+const createLogo = require('./library/generateLogo')
 const { default: Choices } = require('inquirer/lib/objects/choices.js');
-// const util = require('util')
-// const promiseFs = util.promisify(fs.writeFile)
-
-// async function init() {
-//     let data;
-//     try {
-//          data = await inquirer.prompt([{
-//             type: 'input',
-//             message: 'Type in up to 3 letters for your logo:',
-//             name: 'initials'
-//         }, {
-//             type: 'input',
-//             message: 'Type a color for the text on your logo:',
-//             name: 'textColor'
-//         }, {
-//             type: 'input',
-//             message: 'Type a background color for your logo:',
-//             name: 'background'
-//         }, {
-//             type: 'list',
-//             message: 'Choose a shape for your logo:',
-//             choices: ['Circle', 'Square', 'Triangle'],
-//             name: 'shape'
-//         }
-//         ])
-//     } catch (error) {
-//         console.error(error)
-//     }
 
 
 // prompts on CLI for user to input answers
@@ -59,6 +31,38 @@ const entries = inquirer.prompt([{
         if (err) console.error(err);
         else console.log('Generated logo.svg');
   })});
+
+
+
+
+  // const util = require('util')
+// const promiseFs = util.promisify(fs.writeFile)
+
+// async function init() {
+//     let data;
+//     try {
+//          data = await inquirer.prompt([{
+//             type: 'input',
+//             message: 'Type in up to 3 letters for your logo:',
+//             name: 'initials'
+//         }, {
+//             type: 'input',
+//             message: 'Type a color for the text on your logo:',
+//             name: 'textColor'
+//         }, {
+//             type: 'input',
+//             message: 'Type a background color for your logo:',
+//             name: 'background'
+//         }, {
+//             type: 'list',
+//             message: 'Choose a shape for your logo:',
+//             choices: ['Circle', 'Square', 'Triangle'],
+//             name: 'shape'
+//         }
+//         ])
+//     } catch (error) {
+//         console.error(error)
+//     }
 
 //    try {
 //     await promiseFs(filePath, svgContent)
