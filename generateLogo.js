b649adade327165f1cada52aff55530972e7b88e
Saydeezr <saydeezr@gmail.com>
2024-05-13 12:53:22 -0400

    changed asyn format back to promise and fixed shape layout

--- generateLogo.js
+++ generateLogo.js
@@ -2,23 +2,23 @@ const createLogo = (data) => {
     const { initials, textColor, background, shape } = data;
 
     //implement user's initials + text color
-    const textElement = `<text x="50" y="50" fill="${textColor}">${initials}</text>`
+    const textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${initials}</text>`
 
     //imlement user's shape + background color
     let shapeElement;
         if (shape === 'Circle'){
            shapeElement = `<circle fill="${background}" cx="150" cy="100" r="80"/>`
-        } else if (shape === 'Rectangle'){
-           shapeElement = `<rect fill="${background}" x="100" y="100" height="50" width="50"/>`
+        } else if (shape === 'Square'){
+           shapeElement = `<rect fill="${background}" x="50" y="15" height="200" width="200"/>`
         } else {
-           shapeElement = `<polygon fill="${background}" points="250,60 100,400 400,400"/>`
+           shapeElement = `<polygon fill="${background}" height='100%' width='100%' points="0,200 300,200 150,0"/>`
         }
  
     //take both elements and formulate the logo
     const svgContent = `
-    <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-    ${textElement}
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     ${shapeElement}
+    ${textElement}
     </svg>`;
 
     return svgContent;
--- index.js
+++ index.js
@@ -2,71 +2,63 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const createLogo = require('./generateLogo')
 const { default: Choices } = require('inquirer/lib/objects/choices.js');
-const util = require('util')
-const promiseFs = util.promisify(fs.writeFile)
+// const util = require('util')
+// const promiseFs = util.promisify(fs.writeFile)
 
-async function init() {
-    try {
-        const data = await inquirer.prompt([{
-            type: 'input',
-            message: 'Type in up to 3 letters for your logo:',
-            name: 'initials'
-        }, {
-            type: 'input',
-            message: 'Type a color for the text on your logo:',
-            name: 'textColor'
-        }, {
-            type: 'input',
-            message: 'Type a background color for your logo:',
-            name: 'background'
-        }, {
-            type: 'list',
-            message: 'Choose a shape for your logo:',
-            choices: ['Circle', 'Square', 'Triangle'],
-            name: 'shape'
-        }
-        ])
-    } catch (error) {
-        console.error(error)
-    }
+// async function init() {
+//     let data;
+//     try {
+//          data = await inquirer.prompt([{
+//             type: 'input',
+//             message: 'Type in up to 3 letters for your logo:',
+//             name: 'initials'
+//         }, {
+//             type: 'input',
+//             message: 'Type a color for the text on your logo:',
+//             name: 'textColor'
+//         }, {
+//             type: 'input',
+//             message: 'Type a background color for your logo:',
+//             name: 'background'
+//         }, {
+//             type: 'list',
+//             message: 'Choose a shape for your logo:',
+//             choices: ['Circle', 'Square', 'Triangle'],
+//             name: 'shape'
+//         }
+//         ])
+//     } catch (error) {
+//         console.error(error)
+//     }
 
 
+// prompts on CLI for user to input answers
+const entries = inquirer.prompt([{
+    type: 'input',
+    message: 'Type in up to 3 letters for your logo:',
+    name:'initials'
+}, {
+    type: 'input',
+    message: 'Type a color for the text on your logo:',
+    name: 'textColor'
+}, {
+    type: 'input',
+    message: 'Type a background color for your logo:',
+    name: 'background'
+}, {
+    type: 'list',
+    message: 'Choose a shape for your logo:',
+    choices: ['Circle', 'Square', 'Triangle'],
+    name: 'shape'
+}
+])  //after entries are filled, do the work to get back the logo
+.then(async(data) => {
     const svgContent = createLogo(data)
     const filePath = './library/logo.svg'
-
-    try {
-        await promiseFs(filePath, svgContent)
-    } catch (error) {
-        console.error(error)
-    }
-}
-//prompts on CLI for user to input answers
-// const entries = inquirer.prompt([{
-//     type: 'input',
-//     message: 'Type in up to 3 letters for your logo:',
-//     name:'initials'
-// }, {
-//     type: 'input',
-//     message: 'Type a color for the text on your logo:',
-//     name: 'textColor'
-// }, {
-//     type: 'input',
-//     message: 'Type a background color for your logo:',
-//     name: 'background'
-// }, {
-//     type: 'list',
-//     message: 'Choose a shape for your logo:',
-//     choices: ['Circle', 'Square', 'Triangle'],
-//     name: 'shape'
-// }
-// ])  //after entries are filled, do the work to get back the logo
-// .then(async(data) => {
-//     const svgContent = createLogo(data)
-//     const filePath = './library/logo.svg'
-//     fs.writeFile(filePath, svgContent, (err) => {
-//         if (err) console.error(err);
-//         else console.log('logo created successfuly');
-//   })
+    fs.writeFile(filePath, svgContent, (err) => {
+        if (err) console.error(err);
+        else console.log('Generated logo.svg');
+  })});
 
 //    try {
 //     await promiseFs(filePath, svgContent)
@@ -75,4 +67,4 @@ async function init() {
 //    }
 // });
 
-init();
+// init();
