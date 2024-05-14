e4820d8335e37fd0db31dc3f4158e2735360ce85
Saydeezr <saydeezr@gmail.com>
2024-05-14 13:43:19 -0400

    made some syntax fixes

--- library/shapes.js
+++ library/shapes.js
@@ -3,25 +3,25 @@ class Shape {
         this.background ='';
     }
     setColor(background) {
-        this.background = (background);
+        this.background = background;
     }
 }
 
 class Circle extends Shape{
     render(){
-        return `<circle fill="${background}" cx="150" cy="100" r="80"/>`
+        return `<circle fill="${this.background}" cx="150" cy="100" r="80"/>`
     }
 }
 
 class Square extends Shape{
     render(){
-        return `<rect fill="${background}" x="50" y="15" height="200" width="200"/>`
+        return `<rect fill="${this.background}" x="50" y="15" height="200" width="200"/>`
     }
 }
 
 class Triangle extends Shape{ 
     render(){
-        return `<polygon fill="${background}" height='100%' width='100%' points="0,200 300,200 150,0"/>`
+        return `<polygon fill="${this.background}" height='100%' width='100%' points="0,200 300,200 150,0"/>`
     }
 }
 module.exports = { Circle, Square, Triangle }
